3fc3b068bb7bf4ec5d1f3913004f17042e38cf7a
Shini <halimshini@hotmail.com>
2023-03-09 19:28:09 -0500

    added classes Triangle,Circle,Square

---
+++ Lib/shapes.js
@@ -0,0 +1,21 @@
+class Triangle {
+    constructor(){
+
+    }
+}
+
+class  Circle{
+    constructor(){
+
+    }
+}
+
+class Square{
+    constructor(){
+
+    }
+}
+
+module.export=Triangle;
+module.export=Circle;
+module.export=Square;
